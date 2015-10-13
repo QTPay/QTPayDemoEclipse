bdc4a1a40cf00256b45ad9b1d4586dbc47b4f5f6
chenfeiyue <chenfeiyue@qfpay.com>
2015-10-13 10:58:51 +0800

    delete

--- QTPayDemo/src/com/example/qianfangdemo/Utils/Toaster.java
+++
@@ -1,1 +0,0 @@
-package com.example.qianfangdemo.Utils;import android.content.Context;import android.widget.Toast;public class Toaster {	public static void show(Context context, String content) {		Toast.makeText(context, content, Toast.LENGTH_SHORT).show();	}}
